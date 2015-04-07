6b0fbfd29153901c5e2546574848abcf0358b990
Stephanie <stephaniebarreyro@gmail.com>
2015-04-07 16:57:11 -0700

    FUNCTION - Add winning condition

---
+++ public/js/game.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  // arena = new Arena(600, 1200);
+  player1 = new Karina(10, 10);
+  player2 = new Stef(10, 300);
+
+  Mousetrap.bind('a', function(event) {
+    player1.move();
+  });
+
+  Mousetrap.bind('l', function(event) {
+    player2.move();
+  });
+
+  setInterval(function() {
+    if (player1.x == 1000) {
+      $(".karinaWins").css("display", "block");
+      Mousetrap.unbind('l');
+    };
+  });
+
+   setInterval(function() {
+    if (player2.x == 1000) {
+      $(".StephWins").css("display", "block");
+      Mousetrap.unbind('a');
+      };
+  });
+})
